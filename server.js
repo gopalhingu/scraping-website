fe786e4f1c7cdfbcdc1d1ed5db9da7de7065be36
gopalhingu <gopalhingu123@gmail.com>
2026-01-08 12:54:29 +0530

    `Update puppeteer launch options`

--- server.js
+++ server.js
@@ -62,7 +62,17 @@ async function scrapePlaywright(url) {
  * Puppeteer scraper
  */
 async function scrapePuppeteer(url) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+    // headless: true,
+    headless: "new",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--single-process"
+    ]
+  });
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle2' });
